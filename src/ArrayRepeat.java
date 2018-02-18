7525e77f4e1cc29bed5e112e90aab687b3613917
Vijay Sonani <vijay.sonani@cerner.com>
2018-02-18 11:09:18 -0600

    Update array questions

--- src/ArrayRepeat.java
+++ src/ArrayRepeat.java
@@ -1,23 +1,24 @@
 import java.util.HashSet;
 
 /*
-Given a read only array of n + 1 integers between 1 and n, find one number that repeats 
-in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
-
-Sample Input:
-
-[3 4 1 4 1]
-Sample Output:
-
-1
-If there are multiple possible answers ( like in the sample case above ), output any one.
-
-If there is no duplicate, output -1 
-
-Notes:
-array of n + 1 integers between 1 and n == atleast one number duplicate number
-O(n) space == can copy all elements into another data structure (set in this case)
- */
+* Given a read only array of n + 1 integers between 1 and n, find one number that repeats 
+* in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
+*
+* Sample Input:
+* [3 4 1 4 1]
+*
+* Sample Output:
+* 1
+*
+* If there are multiple possible answers ( like in the sample case above ), output any one.
+*
+* If there is no duplicate, output -1 
+*
+* Notes:
+* array of n + 1 integers between 1 and n == atleast one number duplicate number
+* O(n) space == can copy all elements into another data structure (set in this case)
+*
+*/
 
  public class ArrayRepeat {
 	public static void main(String args[]) {
---
+++ src/Excel.java
@@ -0,0 +1,26 @@
+/**
+ * Given a column title as appears in an Excel sheet, return its corresponding column number.
+ * Example:
+ *
+ *    A -> 1 
+ *    B -> 2 
+ *    C -> 3
+ *    
+ *    ...
+ *    
+ *    Z -> 26
+ *    
+ *    AA -> 27
+ *    
+ *    AB -> 28
+ *  
+ * 
+ * Input Output 
+ * 	26 		Z 
+ * 	51 		AY 
+ * 	52 		AZ 
+ * 	80 		CB 
+ * 	676 	YZ 
+ * 	702 	ZZ 
+ * 	705 	AAC
+ */
