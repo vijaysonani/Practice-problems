b0008eb606f865424f6db848790241dab57bea00
Vijay Sonani <vijay.sonani@cerner.com>
2018-02-18 10:58:51 -0600

    Initial commit

---
+++ src/ArrayDuplicates.java
@@ -0,0 +1,36 @@
+import java.util.HashSet;
+
+/**
+ * 
+ * Remove Element
+ * 
+ * Given an array and a value, remove all the instances of that value in the
+ * array. Also return the number of elements left in the array after the
+ * operation. It does not matter what is left beyond the expected length.
+ * 
+ * Example: If array A is [4, 1, 1, 2, 1, 3]
+ * and value elem is 1, then new length is 3, and A is now [4, 2, 3]
+ * 
+ * Try to do it in less than linear additional space complexity.
+ * 
+ *
+ */
+public class ArrayDuplicates {
+    public static void main(String args[]) {
+		int[] arr = {4, 1, 1, 2, 1, 3};
+		ArrayRepeat obj = new ArrayRepeat();
+		obj.findDuplicate(arr);
+    }
+    
+    public void findDuplicate(int[] arr) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int var : arr) {
+			if (set.contains(var)) {
+				System.out.println(var);
+				break;
+			} else {
+				set.add(var);
+			}
+		}
+	}
+}
---
+++ src/ArrayRepeat.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+
+/*
+Given a read only array of n + 1 integers between 1 and n, find one number that repeats 
+in linear time using less than O(n) space and traversing the stream sequentially O(1) times.
+
+Sample Input:
+
+[3 4 1 4 1]
+Sample Output:
+
+1
+If there are multiple possible answers ( like in the sample case above ), output any one.
+
+If there is no duplicate, output -1 
+
+Notes:
+array of n + 1 integers between 1 and n == atleast one number duplicate number
+O(n) space == can copy all elements into another data structure (set in this case)
+ */
+
+ public class ArrayRepeat {
+	public static void main(String args[]) {
+		int[] arr = {3, 4, 1, 4, 1};
+		ArrayRepeat obj = new ArrayRepeat();
+		obj.findDuplicate(arr);
+	}
+
+	public void findDuplicate(int[] arr) {
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int var : arr) {
+			if (set.contains(var)) {
+				System.out.println(var);
+				break;
+			} else {
+				set.add(var);
+			}
+		}
+	}
+}
