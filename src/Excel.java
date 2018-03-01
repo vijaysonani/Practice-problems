dfd7ceb51bacbf190ee1c71301a0a84c0e48478c
Vijay Sonani <vijay.sonani@cerner.com>
2018-02-28 22:07:48 -0600

    ExcelTitle

--- src/Excel.java
+++ src/Excel.java
@@ -24,3 +24,24 @@
  * 	702 	ZZ 
  * 	705 	AAC
  */
+
+public class Excel{
+  public static void main (String args[]){
+    System.out.println("Excel");
+    System.out.println(new Excel().convert(54));
+  }
+
+  public String convert(int A){
+    StringBuilder sb = new StringBuilder();
+ 
+    while(A > 0){
+        A--;
+        char ch = (char) (A % 26 + 'A');
+        A/= 26;
+        sb.append(ch);
+    }
+ 
+    sb.reverse();
+    return sb.toString();
+  }
+}
