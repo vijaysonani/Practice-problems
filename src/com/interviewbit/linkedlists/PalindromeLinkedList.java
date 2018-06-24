684505b983a615f2271314dde725fe3282f4e091
Vijay Sonani <vijay.sonani@cerner.com>
2018-06-23 21:37:08 -0500

    Palindrome linked list

--- src/com/interviewbit/linkedlists/PalindromeLinkedList.java
+++ src/com/interviewbit/linkedlists/PalindromeLinkedList.java
@@ -23,13 +23,60 @@ public class PalindromeLinkedList extends ListNode {
         node2.next = node3;
         node3.next = node4;
 
-        ListNode result = new PalindromeLinkedList().palindromeList(node1);
-        System.out.println(" kReversed list:");
-        printList(result);
+        int result = new PalindromeLinkedList().lPalin(node1);
+        System.out.println("Palindrome list: " + result);
     }
 
-    public ListNode palindromeList(ListNode A) {
+    public int lPalin(ListNode A) {
+        ListNode B = copyList(A);
+        B = reverseList(B);
 
-        return null;
+        while (A != null && B != null) {
+            if (A.val == B.val) {
+                A = A.next;
+                B = B.next;
+            } else {
+                return 0;
+            }
+        }
+
+        return (A == null && B == null) ? 1 : 0;
+    }
+
+    private ListNode reverseList(ListNode A) {
+        ListNode currentNode = A;
+        ListNode nextNode = null;
+        ListNode prevNode = null;
+
+        while (currentNode != null) {
+            nextNode = currentNode.next;
+            currentNode.next = prevNode;
+            prevNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        return prevNode;
+    }
+
+    private ListNode copyList(ListNode A) {
+        ListNode B = null;
+        ListNode ANode = A;
+        ListNode BNode = null;
+        ListNode start = null;
+
+        if (A != null) {
+            B = new ListNode(ANode.val);
+            start = B;
+        }
+
+        while (ANode.next != null) {
+            BNode = new ListNode(ANode.next.val);
+            ANode = ANode.next;
+
+            B.next = BNode;
+            B = B.next;
+        }
+
+        return start;
     }
 }
